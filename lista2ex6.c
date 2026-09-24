1534f188493ddbf2df286bdc0f666901b98e5e30
henry <henrysasakura@gmail.com>
2026-09-24 16:00:26 -0300

    Add files via upload

---
+++ lista2ex6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h> 
+
+int main() {
+
+int numero;
+    
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &numero);
+    
+    printf("Antecessor: %d\n", numero - 1);
+    printf("Sucessor: %d\n\n", numero + 1);
+    
+    return 0;
+}
